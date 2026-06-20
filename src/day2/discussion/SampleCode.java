99bc14c3aa2859b79917327da087052bd88d731c
joySisSai <sj2.jvillamor26@joysistvi.edu.ph>
2026-06-20 15:27:42 +0800

    Add project files: IntelliJ config and day2/day3 sample code

---
+++ src/day2/discussion/SampleCode.java
@@ -0,0 +1,7 @@
+package day2.discussion;
+
+//public class SampleCode {
+//    static void main() {
+//        System.out.println("Hello World");
+//    }
+//}
---
+++ src/day3/discussion/SampleCode.java
@@ -0,0 +1,7 @@
+package day3.discussion;
+
+public class SampleCode {
+    static void main() {
+        System.out.println("Hello World day 3");
+    }
+}
