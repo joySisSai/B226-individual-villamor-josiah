e535f11092416c87d5689e39f11a3de4e30a0ade
joySisSai <sj2.jvillamor26@joysistvi.edu.ph>
2026-06-22 16:50:19 +0800

    day4 dataType discussion

---
+++ src/day4/discussion/DataType.java
@@ -0,0 +1,15 @@
+package day4.discussion;
+
+public class DataType {
+    boolean isActive = true;
+    char letter = 'J';
+    byte num = 50;
+    short num2 = 200;
+    int num3 = 10_000_000;
+    long num4 = 100_000_00;
+    float temp = 35.3f;
+    double average = 90.232324;
+
+    String word = "Java";
+
+}
