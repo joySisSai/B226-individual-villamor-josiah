2b4896dfcfceebc6acc366c4a0d0ca1efee1a0eb
joySisSai <sj2.jvillamor26@joysistvi.edu.ph>
2026-06-22 16:14:22 +0800

    create mini activity

---
+++ src/day4/discussion/Variable.java
@@ -0,0 +1,24 @@
+package day4.discussion;
+
+public class Variable {
+
+    static void main(String[] args) {
+        //initialization
+        //dataType -- variable -- value
+        String h20 = "tubig";
+        int age = 27, age1=23, age2=32, age3=44;
+
+        //declaration
+        double myTemp;
+
+        //assignment
+        myTemp= 35.2;
+
+//        System.out.println("Age: " + age);
+
+        String firstName = "Josiah", middleInitial = "D.", lastName = "Villamor";
+
+        System.out.println(firstName+" "+ middleInitial +" "+lastName);
+
+    }
+}
