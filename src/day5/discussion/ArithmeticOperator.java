4c6bb94573ccf7d75a89503f6eb14b8035668179
joySisSai <sj2.jvillamor26@joysistvi.edu.ph>
2026-06-23 15:20:05 +0800

    day5: add ArithmeticOperator discussion example

---
+++ src/day5/discussion/ArithmeticOperator.java
@@ -0,0 +1,17 @@
+package day5.discussion;
+
+public class ArithmeticOperator {
+    static void main(String[] args) {
+        //+ - * / %
+
+        int x = 5;
+        int y = 2;
+
+        int sum = x + y;
+
+        System.out.println("Sum: " + sum);
+        System.out.println(x - y);
+        System.out.println(x / y);
+        System.out.println(x % y);
+    }
+}
