4819bf8629cbd8b38340fc0413ffc76ab79f1bee
joySisSai <sj2.jvillamor26@joysistvi.edu.ph>
2026-06-24 16:19:10 +0800

    add code for day 6 discussion

---
+++ src/day6/discussion/RelationalOperator.java
@@ -0,0 +1,16 @@
+package day6.discussion;
+
+public class RelationalOperator {
+    static void main(String[] args) {
+        // < > <= >= == != returns boolean
+        int x = 1;
+        int y = 2;
+
+        System.out.println(x == y);
+        System.out.println(x != y);
+        System.out.println(x >= y);
+        System.out.println(x <= y);
+        System.out.println(x < y);
+        System.out.println(x > y);
+    }
+}
