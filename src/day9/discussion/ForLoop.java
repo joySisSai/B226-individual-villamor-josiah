308c2bc8c20d8c27757c8879d5de66ac8bee0a47
joySisSai <sj2.jvillamor26@joysistvi.edu.ph>
2026-06-29 14:43:41 +0800

    add code for day 9 discussion ForLoop

---
+++ src/day9/discussion/ForLoop.java
@@ -0,0 +1,10 @@
+package day9.discussion;
+
+public class ForLoop {
+    static void main(String[] args) {
+        // for loop: initialization; condition; update
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Count: " + i);
+        }
+    }
+}
